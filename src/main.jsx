3b34d0359f8434e60299f5c62ef64571adacdc35
malikkanika9 <97448286+malikkanika9@users.noreply.github.com>
2022-05-30 10:14:40 +0530

    done

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  
-  <React.StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-  </React.StrictMode>
-)
+import { BrowserRouter } from "react-router-dom";
+import { AuthContextProvider } from "./contexts/AuthContext";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+   <React.StrictMode>
+      <AuthContextProvider>
+         <BrowserRouter>
+            <App />
+         </BrowserRouter>
+      </AuthContextProvider>
+   </React.StrictMode>
+);
