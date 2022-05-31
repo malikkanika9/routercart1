d90bf4dbf745f0f7f9df979b867e160cd7f40c3c
malikkanika9 <97448286+malikkanika9@users.noreply.github.com>
2022-05-31 22:33:43 +0530

    completed

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -2,11 +2,13 @@ import React, { useContext, useEffect, useState } from "react";
 import { Navigate, useParams } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext";
 import "./productDetails.css";
+import axios from "axios";
 
 const ProductDetails = () => {
    const { id } = useParams();
    const [data, setData] = useState({});
    const { isAuth } = useContext(AuthContext);
+   const [prod,setProd]=useState([])
 
    const fetchData = async () => {
       let fetched = await fetch(`http://localhost:8080/store/${id}`);
@@ -22,15 +24,22 @@ const ProductDetails = () => {
    if (!isAuth) {
       return <Navigate to={"/login"} />;
    }
+   
    return (
-      <div className="card">
-         <img src={data.image} alt="" />
-         <div className="cardDetails">
-            <p className="biggerText">{data.model}</p>
+ <div className="card">
+  <img src={data.image} alt="" />
+ <div className="cardDetails">
+  <p className="biggerText">{data.model}</p>
             <p>{data.price}</p>
             <p>{data.details}</p>
-            <button className="detailsBtn">Add to cart</button>
+          <button className="detailsBtn" onClick={()=>{
+axios.post("http://localhost:8080/cart",data),
+alert("added to cart")
+console.log(data)
+}} >Add to cart</button>    
          </div>
+
+         
       </div>
    );
 };
--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,11 +1,39 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 export const ShoppingCart=()=>{
+
+    const [data,setData]=useState([])
+useEffect(()=>{
+
+    axios({
+
+        url:"http://localhost:8080/cart",
+        method: "GET"
+    })
+    .then((res)=>{
+        setData(res.data)
+    })
+},[])
+console.log(data)
 
 return (
 
-    <div>
-<h1>Cart</h1>
+    <div  style={{gap:"2rem", marginTop:"10px"}}>
+        {data.map((item)=>(<div>
 
-    </div>
+            <div ><img src={item?.image} style={{width:"50px", height:"50px"}} /></div>
+<div>{item?.details}</div>
+<div>{item?.model}</div>
+ <div> Rs{item?.price} </div>  
+ <button>Remove Item</button>
+
+        </div>))}
+</div>
+
+
+
+  
 )
 
 }
